45145e64cd9afa211429827fb4f2d90c3fc0671b
Prem Kumar shah <151224169+premkr79@users.noreply.github.com>
2025-04-03 21:08:32 +0000

    vaiable config

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "sahpremkr79@gamil.com"
+var accountPassword = "123456"
+accountCity = "Graeter Noida"
+
+//accountId = 2// not allowed
+
+accountEmail = "hchc@.com"
+accountPassword = "12345"
+accountCity = "noida"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
